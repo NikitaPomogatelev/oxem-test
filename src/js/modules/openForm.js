184f74c827421b016548380cf39db0eb3e054a3b
Nikita Pomogatelev <nikitoshelper@mail.ru>
2021-04-08 21:33:53 +0500

    Правки по адаптиву

--- src/js/modules/openForm.js
+++ src/js/modules/openForm.js
@@ -1,25 +1,32 @@
 const openForm = () => {
+    const pageContent = document.querySelector('.page-content');
     const $overlay = document.querySelector('.overlay');
     const btnRequest = document.querySelectorAll('.btn-request');
     const onlineForm = document.querySelector('.online');
     const onlineClose = document.querySelector('.online-close');
 
+    const addActiveClasses = () => {
+        onlineForm.classList.add('active');
+        $overlay.classList.add('active');
+        pageContent.classList.add('active');
+    }
+    const removeActiveClasses = () => {
+        onlineForm.classList.remove('active');
+        $overlay.classList.remove('active');
+        pageContent.classList.remove('active');
+    }
 
     btnRequest.forEach(btn => {
-        btn.addEventListener('click', () => {
-            onlineForm.classList.add('active');
-            $overlay.classList.add('active');
-        });
+        if (!pageContent.classList.contains('active')) {
+            btn.addEventListener('click', () => {
+                addActiveClasses();
+            });
+
+        }
     })
 
-    onlineClose.addEventListener('click', () => {
-        onlineForm.classList.remove('active');
-        $overlay.classList.remove('active');
-    });
-    $overlay.addEventListener('click', () => {
-        onlineForm.classList.remove('active');
-        $overlay.classList.remove('active');
-    });
+    onlineClose.addEventListener('click', removeActiveClasses);
+    $overlay.addEventListener('click', removeActiveClasses);
 }
 
 export default openForm;
